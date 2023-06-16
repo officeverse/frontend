1d55fb6c7dbca801e9e7d60da851b298b398a80f
gracelimshiern <98506014+gracelimshiern@users.noreply.github.com>
2023-06-16 22:18:11 +0800

    Add AI page

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import Forum from "./screens/Forum";
+import AiPal from "./screens/AiPal";
 
 export default function App() {
   return (
     // <View className="flex-1 items-center justify-center">
     <View className="flex-1 items-center">
-      <Forum />
+      <AiPal />
       <StatusBar style="auto" />
     </View>
   );
