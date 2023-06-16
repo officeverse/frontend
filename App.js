dc2161ce46002925c2fa1b1e11d6cc171b9f1c18
Aden Teo <88359484+adenteo@users.noreply.github.com>
2023-06-16 22:35:11 +0800

    Create challenge component

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Rewards from "./screens/Rewards";
+import Challenges from "./screens/Challenges";
 
 export default function App() {
-  return (
-    // <View className="flex-1 items-center justify-center">
-    <View className="flex-1 items-center">
-      <Rewards />
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        // <View className="flex-1 items-center justify-center">
+        <View className="flex-1 items-center">
+            <Challenges />
+            <StatusBar style="auto" />
+        </View>
+    );
 }
