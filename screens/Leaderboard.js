7ddc2bbd27574e251bc1fd1a73c578152e730331
ethan-cyj <e0773997@u.nus.edu>
2023-06-20 00:55:29 +0800

    updated leaderboard and home

--- screens/Leaderboard.js
+++ screens/Leaderboard.js
@@ -1,17 +1,37 @@
-import { SafeAreaView, ImageBackground } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView, ImageBackground, ScrollView } from "react-native";
+import { Menu, Button } from 'react-native-paper';
 
 const image = require("../assets/office_home.png");
 
 export default function Leaderboard() {
+  const [visible, setVisible] = useState(false);
+
+  const openMenu = () => setVisible(true);
+
+  const closeMenu = () => setVisible(false);
+
   return (
     <SafeAreaView style={{flex: 1}}>
-      <ImageBackground
-        source={image}
-        style={{flex: 1}}
-        resizeMode="cover"
-      >
-        {/* Here you will include the scrollable content of your leaderboard */}
-      </ImageBackground>
+      <ScrollView>
+        <ImageBackground
+          source={image}
+          style={{flex: 1}}
+          resizeMode="cover"
+        >
+          {/* Here you will include the scrollable content of your leaderboard */}
+          <Menu
+            visible={visible}
+            onDismiss={closeMenu}
+            anchor={<Button onPress={openMenu}>Leaderboard</Button>}
+          >
+            <Menu.Item onPress={() => {}} title="Item 1" />
+            <Menu.Item onPress={() => {}} title="Item 2" />
+            <Menu.Item onPress={() => {}} title="Item 3" />
+          </Menu>
+          {/* More contents here */}
+        </ImageBackground>
+      </ScrollView>
     </SafeAreaView>
   );
 }
