1aa1355c37a47d90c1963ed285c65679930691b1
Jin Xuan Ow Yong <jinxuan.owyong@gmail.com>
2023-06-20 01:12:20 +0800

    setup aws amplify with auth

--- App.js
+++ App.js
@@ -2,6 +2,10 @@ import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
 import OnboardingStack from './navigation/OnboardingStack';
+import { Amplify } from 'aws-amplify';
+import awsExports from './src/aws-exports';
+
+Amplify.configure(awsExports);
 
 export default function App() {
   return (
---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,27 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "AdminQueries": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "auth": {
+    "officeverse": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "AdminQueries7855c86a": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
