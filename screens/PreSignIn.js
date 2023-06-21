cacc731fee1becad4e6dca7b419837bc7cfe1f19
Aden Teo <88359484+adenteo@users.noreply.github.com>
2023-06-21 15:06:33 +0800

    Fix directing of user when is employee

--- screens/PreSignIn.js
+++ screens/PreSignIn.js
@@ -1,78 +1,84 @@
-import { Alert, ImageBackground } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { Text, View } from 'react-native';
-const image = require('../assets/background.png');
-import LoginForm from '../components/LoginForm';
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
-import { Auth } from 'aws-amplify';
-import { loginUser, setUsername } from '../src/features/authSlice';
-import { useDispatch, useSelector } from 'react-redux';
+import { Alert, ImageBackground } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Text, View } from "react-native";
+const image = require("../assets/background.png");
+import LoginForm from "../components/LoginForm";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { Auth } from "aws-amplify";
+import { loginUser, setUsername } from "../src/features/authSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function PreSignIn({ navigation }) {
-  const hasCompletedOnboarding = useSelector(
-    (state) => state.auth.user.hasCompletedOnboarding
-  );
-  const dispatch = useDispatch();
+    const hasCompletedOnboarding = useSelector(
+        (state) => state.auth.user.hasCompletedOnboarding
+    );
+    const dispatch = useDispatch();
 
-  const onLogin = async (data) => {
-    const { username, password } = data;
-    Auth.signIn(username, password)
-      .then((user) => {
-        const { attributes, username } = user;
-        const {
-          email,
-          family_name: lastName,
-          given_name: firstName,
-          sub,
-        } = attributes;
-        dispatch(
-          loginUser({
-            sub,
-            isEmailVerified: true,
-            attributes: { email, lastName, firstName, username },
-          })
-        );
-        navigation.reset({
-          index: 0,
-          routes: [
-            { name: hasCompletedOnboarding ? 'Home' : 'OnboardingMain' },
-          ],
-        });
-      })
-      .catch(async (err) => {
-        if (err.message === 'User is not confirmed.') {
-          dispatch(setUsername(username));
-          // resend code
-          await Auth.resendSignUp(username).catch((err) => {
-            Alert.alert('Error resending code: ' + err);
-          });
-          navigation.reset({
-            index: 0,
-            routes: [{ name: 'SignUpConfirmEmail' }],
-          });
-          return;
-        }
-        Alert.alert('Error: ' + err);
-      });
-  };
+    const onLogin = async (data) => {
+        const { username, password } = data;
+        Auth.signIn(username, password)
+            .then((user) => {
+                const { attributes, username } = user;
+                const {
+                    email,
+                    family_name: lastName,
+                    given_name: firstName,
+                    sub,
+                } = attributes;
+                dispatch(
+                    loginUser({
+                        sub,
+                        isEmailVerified: true,
+                        attributes: { email, lastName, firstName, username },
+                    })
+                );
+                navigation.reset({
+                    index: 0,
+                    routes: [
+                        {
+                            name: hasCompletedOnboarding
+                                ? "NavBar"
+                                : "OnboardingMain",
+                        },
+                    ],
+                });
+            })
+            .catch(async (err) => {
+                if (err.message === "User is not confirmed.") {
+                    dispatch(setUsername(username));
+                    // resend code
+                    await Auth.resendSignUp(username).catch((err) => {
+                        Alert.alert("Error resending code: " + err);
+                    });
+                    navigation.reset({
+                        index: 0,
+                        routes: [{ name: "SignUpConfirmEmail" }],
+                    });
+                    return;
+                }
+                Alert.alert("Error: " + err);
+            });
+    };
 
-  return (
-    <KeyboardAwareScrollView
-      resetScrollToCoords={{ x: 0, y: 0 }}
-      scrollEnabled={true}
-    >
-      <ImageBackground
-        source={image}
-        resizeMode="cover"
-        className=" justify-center"
-      >
-        <SafeAreaView className="h-[100vh] mt-8">
-          <View className="flex-row items-center justify-center mt-16">
-            <Text className="text-5xl font-bold color-white">Officeverse.</Text>
-          </View>
-          <LoginForm navigation={navigation} onSubmit={onLogin} />
-        </SafeAreaView>
-      </ImageBackground>
-    </KeyboardAwareScrollView>
-  );
+    return (
+        <KeyboardAwareScrollView
+            resetScrollToCoords={{ x: 0, y: 0 }}
+            scrollEnabled={true}
+        >
+            <ImageBackground
+                source={image}
+                resizeMode="cover"
+                className=" justify-center"
+            >
+                <SafeAreaView className="h-[100vh] mt-8">
+                    <View className="flex-row items-center justify-center mt-16">
+                        <Text className="text-5xl font-bold color-white">
+                            Officeverse.
+                        </Text>
+                    </View>
+                    <LoginForm navigation={navigation} onSubmit={onLogin} />
+                </SafeAreaView>
+            </ImageBackground>
+        </KeyboardAwareScrollView>
+    );
 }
--- src/features/authSlice.js
+++ src/features/authSlice.js
@@ -1,51 +1,52 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialAuthState = {
-  user: {
-    sub: '', // unique identifier
-    isEmailVerified: false,
-    attributes: {
-      email: '',
-      username: '',
+    user: {
+        sub: "", // unique identifier
+        isEmailVerified: false,
+        attributes: {
+            email: "",
+            username: "",
+        },
+        isNewSignUp: false,
+        hasCompletedOnboarding: true,
     },
-    isNewSignUp: false,
-    hasCompletedOnboarding: false,
-  },
 };
 
 export const authSlice = createSlice({
-  name: 'auth',
-  initialState: initialAuthState,
-  reducers: {
-    setEmailVerified: (state) => {
-      state.user.isEmailVerified = true;
+    name: "auth",
+    initialState: initialAuthState,
+    reducers: {
+        setEmailVerified: (state) => {
+            state.user.isEmailVerified = true;
+        },
+        createNewUser: (state, action) => {
+            state.user.sub = action.payload.signUpResult.userSub;
+            state.user.isEmailVerified =
+                action.payload.signUpResult.userConfirmed;
+            state.user.attributes = action.payload.attributes;
+            state.user.isNewSignUp = true;
+        },
+        loginUser: (state, action) => {
+            state.user.sub = action.payload.sub;
+            state.user.attributes = action.payload.attributes;
+        },
+        setUsername: (state, action) => {
+            state.user.attributes.username = action.payload;
+        },
+        logoutUser: (state) => {
+            state = initialAuthState;
+        },
     },
-    createNewUser: (state, action) => {
-      state.user.sub = action.payload.signUpResult.userSub;
-      state.user.isEmailVerified = action.payload.signUpResult.userConfirmed;
-      state.user.attributes = action.payload.attributes;
-      state.user.isNewSignUp = true;
-    },
-    loginUser: (state, action) => {
-      state.user.sub = action.payload.sub;
-      state.user.attributes = action.payload.attributes;
-    },
-    setUsername: (state, action) => {
-      state.user.attributes.username = action.payload;
-    },
-    logoutUser: (state) => {
-      state = initialAuthState;
-    },
-  },
 });
 
 // Action creators are generated for each case reducer function
 export const {
-  createNewUser,
-  setEmailVerified,
-  loginUser,
-  logoutUser,
-  setUsername,
+    createNewUser,
+    setEmailVerified,
+    loginUser,
+    logoutUser,
+    setUsername,
 } = authSlice.actions;
 
 export default authSlice.reducer;
